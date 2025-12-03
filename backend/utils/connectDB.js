5ba5ae7bac4907950bf8c1ca3a01986f048d7559
Sushant-Prasad <sushantprasad77@gmail.com>
2025-12-03 22:27:51 +0530

    fix: remove redundant comment in connectDB.js

--- backend/utils/connectDB.js
+++ backend/utils/connectDB.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config(); // Load .env values
+dotenv.config(); 
 
 // Create a connection tracker object
 const connection = {
